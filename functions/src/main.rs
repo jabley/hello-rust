964c371a65e8dbc403c241ec61450713d0d770ce
James Abley <james.abley@gmail.com>
2017-09-14 14:55:44 +0100

    Statements and expressions
    Plus blocks too!

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,8 +1,10 @@
 fn main() {
-    another_function(5, 6);
-}
+    let x = 5;
 
-fn another_function(x: i32, y: i32) {
-    println!("The value of x is: {}", x);
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
     println!("The value of y is: {}", y);
 }
