877e99a58beb8b15ae806692d0b0c90ae778c1ca
James Abley <james.abley@gmail.com>
2019-03-12 09:59:12 +0000

    Cannot borrow as mutable and immutable
    ```
    error[E0502]: cannot borrow `s` as mutable because it is also borrowed as immutable
     --> src/main.rs:6:19
      |
    4 |     let r1 = &s; // no problem
      |               - immutable borrow occurs here
    5 |     let r2 = &s; // no problem
    6 |     let r3 = &mut s; // BIG PROBLEM
      |                   ^ mutable borrow occurs here
    ...
    9 | }
      | - immutable borrow ends here
    
    error: aborting due to previous error
    ```

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,10 +1,9 @@
 fn main() {
     let mut s = String::from("hello");
 
-    {
-        let r1 = &mut s;
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    let r3 = &mut s; // BIG PROBLEM
 
-    } // r1 goes out of scope here, so we can make a new reference with no problems.
-
-    let r2 = &mut s;
+    println!("{}, {}, and {}", r1, r2, r3);
 }
