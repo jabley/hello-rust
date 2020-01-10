c6d4db9f51e611617a8ba9658a9a02891cff0763
James Abley <james.abley@gmail.com>
2020-01-10 09:26:04 +0000

    Add factory function to create a User

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,12 +1,12 @@
 #![allow(unused_variables)]
-fn main() {
-    struct User {
-        username: String,
-        email: String,
-        sign_in_count: u64,
-        active: bool,
-    }
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
 
+fn main() {
     let mut user1 = User {
         email: String::from("someone@example.com"),
         username: String::from("someusername123"),
@@ -15,4 +15,15 @@ fn main() {
     };
 
     user1.email = String::from("anotheremail@example.com");
+
+    let user = build_user(String::from("alice@example.com"), String::from("alice"));
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email: email,
+        username: username,
+        active: true,
+        sign_in_count: 1,
+    }
 }
