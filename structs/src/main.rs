9330abb1b9fb7a63f76c8a8736ed36c334f13340
James Abley <james.abley@gmail.com>
2020-03-03 08:29:54 +0000

    Attempt to store references in structs
    This fails due to ownership issues:
    
    ```
    error[E0106]: missing lifetime specifier
     --> src/main.rs:3:15
      |
    3 |     username: &str,
      |               ^ expected lifetime parameter
    
    error[E0106]: missing lifetime specifier
     --> src/main.rs:4:12
      |
    4 |     email: &str,
      |            ^ expected lifetime parameter
    
    error: aborting due to 2 previous errors
    ```

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,35 +1,16 @@
 #![allow(unused_variables)]
 struct User {
-    username: String,
-    email: String,
+    username: &str,
+    email: &str,
     sign_in_count: u64,
     active: bool,
 }
 
 fn main() {
-    let mut user1 = User {
-        email: String::from("someone@example.com"),
-        username: String::from("someusername123"),
+    let user1 = User {
+        email: "someone@example.com",
+        username: "someusername123",
         active: true,
         sign_in_count: 1,
     };
-
-    user1.email = String::from("anotheremail@example.com");
-
-    let user = build_user(String::from("alice@example.com"), String::from("alice"));
-
-    let user2 = User {
-        email: String::from("another@example.com"),
-        username: String::from("anotherusername567"),
-        ..user1
-    };
-}
-
-fn build_user(email: String, username: String) -> User {
-    User {
-        email,
-        username,
-        active: true,
-        sign_in_count: 1,
-    }
 }
