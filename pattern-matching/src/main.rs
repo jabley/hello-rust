d73689666f347e37d541dafedc02939e3b206848
James Abley <james.abley@gmail.com>
2020-05-10 13:24:54 +0100

    Show `if let` version of pattern matching
    This is syntax sugar for a match that runs code when the value matches
    one pattern and then ignores all other values.
    
    We are gaining conciseness, but losing exhaustive checking.

--- pattern-matching/src/main.rs
+++ pattern-matching/src/main.rs
@@ -1,8 +1,7 @@
 
 fn main() {
     let some_u8_value = Some(0u8);
-    match some_u8_value {
-        Some(3) => println!("three"),
-        _ => (),
+    if let Some(3) = some_u8_value {
+        println!("three");
     }
 }
