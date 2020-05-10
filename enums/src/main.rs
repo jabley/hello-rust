58d58b5c78d24b1ba069462687bad422183c55d0
James Abley <james.abley@gmail.com>
2020-05-10 12:22:21 +0100

    Define route function which works on enums

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -3,8 +3,14 @@ enum IpAddrKind {
     V6,
 }
 
+fn route(ip_kind: IpAddrKind) {}
+
 fn main() {
     let four = IpAddrKind::V4;
     let six = IpAddrKind::V6;
+
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
+    
     println!("Hello, world!");
 }
