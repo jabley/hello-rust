ebe2e6ac8af85748068da896140fd9b890de1d11
James Abley <james.abley@gmail.com>
2020-05-10 21:55:39 +0100

    Show aliasing imports

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -6,15 +6,15 @@ mod tests {
     }
 }
 
-use std::fmt;
-use std::io;
+use std::fmt::Result;
+use std::io::Result as IoResult;
 
-fn function1() -> fmt::Result {
+fn function1() -> Result {
     // --snip--
     Ok(())
 }
 
-fn function2() -> io::Result<()> {
+fn function2() -> IoResult<()> {
     // --snip--
     Ok(())
 }
