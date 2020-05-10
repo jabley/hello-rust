8d284582ef6ca901b2070eb7edb454f2ab7726e0
James Abley <james.abley@gmail.com>
2020-05-10 12:34:12 +0100

    Use Option enum
    This is part of the language to attempt to manage Tony Hoare's billion
    dollar mistake.

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,18 +1,8 @@
-enum Message {
-    Quit,
-    Move { x: i32, y: i32 },
-    Write(String),
-    ChangeColor(i32, i32, i32),
-}
-
-impl Message {
-    fn call(&self) {
-        // method body would be defined here
-    }
-}
-
 fn main() {
-    let m = Message::Write(String::from("hello"));
-    m.call();
+    let some_number = Some(5);
+    let some_string = Some("a string");
+
+    let absent_number: Option<i32> = None;
+    
     println!("Hello, world!");
 }
