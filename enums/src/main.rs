6f849de9aacc1ab938b4ee1444eeaca2f9f53e88
James Abley <james.abley@gmail.com>
2020-05-10 12:24:20 +0100

    Try to define different IpAddr implementations
    There is a more concise form coming!

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -3,14 +3,26 @@ enum IpAddrKind {
     V6,
 }
 
-fn route(ip_kind: IpAddrKind) {}
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+fn route(ip: IpAddr) {}
 
 fn main() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
 
-    route(IpAddrKind::V4);
-    route(IpAddrKind::V6);
-    
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    route(home);
+    route(loopback);
+
     println!("Hello, world!");
 }
