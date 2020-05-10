656c7bf4bd0ba2a5a418f07d9e523e4b92c7749a
James Abley <james.abley@gmail.com>
2020-05-10 12:32:30 +0100

    Show rich enum possibilities
    This enum has four variants with different types:
    
    * Quit has no data associated with it at all
    * Move includes an anonymous struct inside it
    * Write includes a single String
    * ChangeColor includes three i32 values
    
    We can also define methods on enums.

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,17 +1,18 @@
-enum IpAddr{
-    V4(u8, u8, u8, u8),
-    V6(String),
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
 }
 
-fn route(ip: IpAddr) {}
+impl Message {
+    fn call(&self) {
+        // method body would be defined here
+    }
+}
 
 fn main() {
-    let home = IpAddr::V4(127, 0, 0, 1);
-
-    let loopback = IpAddr::V6(String::from("::1"));
-
-    route(home);
-    route(loopback);
-
+    let m = Message::Write(String::from("hello"));
+    m.call();
     println!("Hello, world!");
 }
