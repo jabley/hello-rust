0f1bc82d11071c71b6d67e7c3a2c8164170e6260
James Abley <james.abley@gmail.com>
2020-05-10 13:30:08 +0100

    Show `if let ... else` pattern match
    This can be a way of ensuring all code paths are still handled.

--- pattern-matching/src/main.rs
+++ pattern-matching/src/main.rs
@@ -1,7 +1,25 @@
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
 
 fn main() {
-    let some_u8_value = Some(0u8);
-    if let Some(3) = some_u8_value {
-        println!("three");
+    let coin = Coin::Penny;
+    let mut count = 0;
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
     }
+
+    println!("count is {:?}", count)
 }
