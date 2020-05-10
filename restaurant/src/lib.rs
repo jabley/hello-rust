fb977682dd810e15ea1445cc465fe8781ac580ef
James Abley <james.abley@gmail.com>
2020-05-10 21:48:04 +0100

    Show how to import stdlib structs and enums
    Use the full path and reference the struct by name.

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -8,7 +8,12 @@ mod tests {
 
 mod front_of_house {
     pub mod hosting {
-        pub fn add_to_waitlist() {}
+        use std::collections::HashMap;
+
+        pub fn add_to_waitlist() {
+            let mut map = HashMap::new();
+            map.insert(1, 2);
+        }
     }
 }
 
