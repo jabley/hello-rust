e2aa240c5478349144cc97342cd0276d2c70f3b4
James Abley <james.abley@gmail.com>
2020-05-10 12:51:24 +0100

    Show destructuring in pattern matching
    Extract the UsState out of a Quarter into the state variable.

--- pattern-matching/src/main.rs
+++ pattern-matching/src/main.rs
@@ -1,8 +1,15 @@
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -13,11 +20,14 @@ fn value_in_cents(coin: Coin) -> u8 {
         },
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
     }
 }
 
 fn main() {
-    let value = value_in_cents(Coin::Penny);
+    let value = value_in_cents(Coin::Quarter(UsState::Alaska));
     println!("value {:?}!", value);
 }
