b4d297752976fc9ad5c0d3b51f214bc696b60bfb
James Abley <james.abley@gmail.com>
2020-06-29 13:58:25 +0100

    Other way of declaring a string

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,8 +1,3 @@
 fn main() {
-    let data = "initial contents";
-
-    let s = data.to_string();
-
-    // the method also works on a literal directly:
-    let s = "initial contents".to_string();
+    let s = String::from("initial contents");
 }
