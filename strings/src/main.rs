b6be2a4f0e0f426231fd2e34986c9d86dcfc7f25
James Abley <james.abley@gmail.com>
2020-06-29 13:57:48 +0100

    Show converting &str to String

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    let mut s = String::new();
+    let data = "initial contents";
+
+    let s = data.to_string();
+
+    // the method also works on a literal directly:
+    let s = "initial contents".to_string();
 }
